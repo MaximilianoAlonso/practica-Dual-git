da2b35abcd8e4ebc1035f1f3bc222b2320d301c5
MaximilianoAlonso <84605497+MaximilianoAlonso@users.noreply.github.com>
2022-11-08 18:10:10 -0300

    agregue una variable

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ const usuariosActivos = require ("./usuariosActivos.json")
 const usuariosInactivos = require("./usuariosInactivos.json")
 console.log("Usuarios activos " + usuariosActivos.length);
 console.log(usuariosActivos);
+const agregarUsuario;
