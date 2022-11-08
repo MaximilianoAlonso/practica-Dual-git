408ed9946294a4f69ec5b99c0da194ad3400b3f2
Maximiliano <elgrungemaxi1986@gmail.com>
2022-11-08 18:07:10 -0300

    agregue cambiols en app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 const usuariosActivos = require ("./usuariosActivos.json")
 const usuariosInactivos = require("./usuariosInactivos.json")
+console.log("Usuarios activos " + usuariosActivos.length);
+console.log(usuariosActivos);
